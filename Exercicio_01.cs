b91b72893facb4cc52dd5c5c2ac384237c6a4e8f
larisssa <larissa.xaraujo@al.infnet.edu.br>
2025-05-13 23:34:30 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio_01.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public delegate decimal CalculateDiscount(decimal precoOriginal);
+
+class Exercicio_01
+{
+    public static void executar()
+    {
+        Console.Write("\nInforme o preço do produto: R$");
+        string? input = Console.ReadLine();
+        if (!decimal.TryParse(input, out decimal precoOriginal))
+        {
+            Console.WriteLine("Valor inválido!");
+            return;
+        }
+
+        CalculateDiscount calcularDesconto = aplicaDesconto;
+
+        decimal precoComDesconto = calcularDesconto(precoOriginal);
+
+        Console.WriteLine($"\nPreço original: {precoOriginal:C}");
+        Console.WriteLine($"Preço com desconto(10%): {precoComDesconto:C}");
+    }
+
+    public static decimal aplicaDesconto(decimal precoOriginal)
+    {
+        decimal desconto = 0.1m;
+        return precoOriginal - (precoOriginal * desconto);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("\nEscolha a opção do exercício que deseja visualizar a resolução");
+            Console.WriteLine("1 - Delegate personalizado para descontos");
+            Console.WriteLine("13 - Sair");
+            Console.Write("\nDigite a opção desejada: ");
+
+            var opcao = Console.ReadLine();
+
+            switch (opcao)
+            {
+                case "1":
+                    Exercicio_01.executar();
+                    break;
+                case "13":
+                    Console.WriteLine("\n\nSaindo...");
+                    return;
+                default:
+                    Console.WriteLine("\n\nOpção inválida. Tente novamente.");
+                    break;
+            }
+        }
+    }
+}
